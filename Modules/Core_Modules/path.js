156c3311f90440dd691bfcc10a119fc06290aefc
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-06-17 20:32:41 +0600

    Update path.js

--- Modules/Core_Modules/path.js
+++ Modules/Core_Modules/path.js
@@ -2,3 +2,7 @@ const path = require('path');
 const dir = '/home/samin/Videos/nodejs/index.js';
 console.log(path.basename(dir));
 console.log(path.parse(dir));
+
+// join path segment together
+const myPath = path.join(__dirname, '/index.js');
+console.log(myPath);
