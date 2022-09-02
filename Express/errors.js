095bd5dbf50edee4196227bd9eb0589039f6673a
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-09-02 21:12:47 +0600

    Update errors.js

--- Express/errors.js
+++ Express/errors.js
@@ -1,3 +1,7 @@
 1. Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client 
 // in a single request we can send single header, but if a header already send & we again try to send another header, this
 // error will arrive.
+
+
+2. Error [ERR_MODULE_NOT_FOUND]: Cannot find module '/home/samin/Videos/Projects/TodoApp/schema/todoSchema' imported from /home/samin/Videos/Projects/TodoApp/routerHandler/todoHandler.js
+// put .js extension in filename
