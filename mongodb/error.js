f38d2bd88bcad0982fe0cbb9cf42c802b2ee0ca0
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-09-02 23:38:18 +0600

    Create error.js

---
+++ mongodb/error.js
@@ -0,0 +1,2 @@
+MongooseError: Query was already executed: Todo.updateOne -> 
+  https://stackoverflow.com/questions/68996929/mongooseerror-query-was-already-executed-todo-updateone-id-new-objectid6
