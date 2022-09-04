bb3693a6820fd310f37dbf4001500934fee6a3ad
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-09-04 12:40:40 +0600

    Update error.js

--- mongodb/error.js
+++ mongodb/error.js
@@ -1,2 +1,3 @@
 MongooseError: Query was already executed: Todo.updateOne -> 
+  use try catch block  
   https://stackoverflow.com/questions/68996929/mongooseerror-query-was-already-executed-todo-updateone-id-new-objectid6
